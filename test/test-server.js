a26a68a436eb806857768cc64a4cc071e57e690a
Camila Salvador <camilasalvador2@gmail.com>
2017-09-26 00:12:45 -0400

    added put and delete tests

--- test/test-server.js
+++ test/test-server.js
@@ -41,8 +41,49 @@ describe("GET endpoint", function(){
 })
 
 
+const updateData = {
+    title: "I Love Tomtatoes",
+    eventType: "milky way"
+  };
+  
+  return Entry
+    .findOne()
+    .then(function(entry) {
+      updateData.id = entry.id;
+  
+      // make request then inspect it to make sure it reflects
+      // data we sent
+      return chai.request(app)
+        .put(`/entries/${entry.id}`)
+        .send(updateData);
+    })
+    .then(function(res) {
+      res.should.have.status(204);
+  
+      return Entry.findById(updateData.id);
+    })
+    .then(function(entry) {
+      entry.title.should.equal(updateData.title);
+      entry.eventType.should.equal(updateData.eventType);
+    });
 
-
+    
+    
+    let entry;
+    
+    Entry
+      .findOne()
+      .then(function(_entry) {
+        entry = _entry;
+        return chai.request(app).delete(`/entries/${entry.id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+        return Entry.findById(entry.id);
+      })
+      .then(function(_entry) {
+        should.not.exist(_entry);
+      });
 
 
     
