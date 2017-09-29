75ed87d6f52d43629f17df34e255d00d322c7891
Camila Salvador <camilasalvador2@gmail.com>
2017-09-28 22:25:51 -0400

    updated schema in models.js file

--- models.js
+++ models.js
@@ -3,10 +3,23 @@
 const mongoose = require("mongoose");
 
 const entrySchema = mongoose.Schema ({
-    title: String,
+    title: {type: String, required: true},
     eventType: String,
     content: {type: String}, 
+    created: {type: Date, default: Date.now}
 });
+
+  
+blogPostSchema.methods.apiRepr = function() {
+    return {
+      id: this._id,
+      title: this.title,
+      eventType: this.eventType,
+      content: this.content,
+      created: this.created
+    }; 
+}
+
 
 const Entry = mongoose.model("Entry", entrySchema);
 
