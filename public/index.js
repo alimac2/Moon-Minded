8d6cd9f601eefd035ef0c1a6eab63ddca95b1935
Camila Salvador <camilasalvador2@gmail.com>
2017-10-21 17:55:37 -0400

    console log

--- public/index.js
+++ public/index.js
@@ -43,27 +43,30 @@ function submitEntryData() {    /* possibly rename */
 };
 
 
-function getEntries(callbackFn) {
+function getEntries() {
     $.ajax({
         method: "GET",
         url: "/entries", 
         dataType: "json",
         contentType: "application/json", 
         success: function(data) {
-            displayEntries(data);
+            // displayEntries(data);
             console.log("GET request works");
         }
     });
 };
 
 function displayEntries(data) {
+
     for (index in data) {
+    console.log(data);
+    console.log(index);
         $(".all-entries").append(
             `<div class='entries-display'>
                 <p>data[index].created</p>
                 <p>data[index].title</p>
                 <button class="edit-btn">Edit</button>
-                <button class='delete-btn'>Delete</button>
+                <button class="delete-btn">Delete</button>
             </div>`
         );  
     };
@@ -74,42 +77,42 @@ function displayEntries(data) {
 // }
 
 
-function deleteEntry() { /*entryId, data, or entryDetails*/
-    $(".delete-entry").click(function() {
-        event.preventDefault();
+// function deleteEntry() { /*entryId, data, or entryDetails*/
+//     $(".delete-entry").click(function() {
+//         event.preventDefault();
         
-        const entryId = $(".delete-btn").val(); /* all delete buttons will have the same class, grabbed from DOM*/
+//         const entryId = $(".delete-btn").val(); /* all delete buttons will have the same class, grabbed from DOM*/
 
-    }); /*review - do you need event in the jquery event callback?*/
+//     }); /*review - do you need event in the jquery event callback?*/
 
-    $.ajax({
-        method: "DELETE",
-        url: "/entries/entryId", /* LOOK OVER */
-        dataType: "json",
-        contentType: "application/json",
-        // data: JSON.stringify(entryDetails),
-        success: function(data) {
-            console.log("DELETE request works");
-        }
-    });
-}
+//     $.ajax({
+//         method: "DELETE",
+//         url: "/entries/entryId", /* LOOK OVER */
+//         dataType: "json",
+//         contentType: "application/json",
+//         // data: JSON.stringify(entryDetails),
+//         success: function(data) {
+//             console.log("DELETE request works");
+//         }
+//     });
+// }
 
 
-function updateEntries(callbackFn) {
-    $.ajax({
-        method: "PUT",
-        url: "/entries/entryId", 
-        dataType: "json",
-        data: entryDetails, /* make sure this is corerct */
-        success: function(data) {
-            console.log("PUT request works");
-        },
-    });
-}
+// function updateEntries(callbackFn) {
+//     $.ajax({
+//         method: "PUT",
+//         url: "/entries/", 
+//         dataType: "json",
+//         data: entryDetails, /* make sure this is corerct */
+//         success: function(data) {
+//             console.log("PUT request works");
+//         },
+//     });
+// }
 
 /* document ready*/
 $(function() {
 submitEntryData();
-deleteEntry();
-
+// deleteEntry();
+getEntries();
 });
