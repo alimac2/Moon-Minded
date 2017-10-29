251949843c5882fb107a3d42d322f4c708ee4680
Camila Salvador <camilasalvador2@gmail.com>
2017-10-29 18:11:37 -0400

    update db url in config file

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       "mongodb://localhost/moon-minded";
+                       "mongodb://ds149324.mlab.com:49324/moon-minded";
+/*"mongodb://localhost/moon-minded";*/
 
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
-	"mongodb://localhost/test-moon-minded");
+    "mongodb://ds155634.mlab.com:55634/test-moon-minded");
+    
+   /*"mongodb://localhost/test-moon-minded";*/
 exports.PORT = process.env.PORT || 8080;
