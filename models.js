3785f53e16f539aaa0d104e46b9c86d9118d0684
Camila Salvador <camilasalvador2@gmail.com>
2017-11-09 22:11:34 -0500

    updated models.js

--- models.js
+++ models.js
@@ -14,8 +14,6 @@ entrySchema.virtual("formattedDate").get(function() {
     return moment(this.created).format("MMMM Do YYYY, hh:mm a");
 });
 
-// .toDate()
-
 /*this is an *instance method* which will be available on all instances
 of the model. This method will be used to return an object that only
 exposes *some* of the fields we want from the underlying data */ 
